1ceed6fd6f07cf325d90f6c0c83031b267306cc5
Josephnkansah <jnkansahoseph4@gmial.com>
2023-11-27 19:35:43 -0800

    Linked list cycle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,13 +2,14 @@
 #define LISTS_H
 
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * 
  */
 typedef struct listint_s
 {
@@ -17,11 +18,8 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int is_palindrome(listint_t **head);
-size_t listint_len(const listint_t *h);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
