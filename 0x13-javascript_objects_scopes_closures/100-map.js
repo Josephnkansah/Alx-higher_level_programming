0541ea64715c4e4cd24be4d28d2737bd7d9e2652
Josephnkansah <jnkansahoseph4@gmail.com>
2024-03-12 15:31:02 -0700

    java

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const map = list.map((x, i) => x * i);
+const list = require('./100-data.js').list;
+
+const newList = list.map((val, idx) => val * idx);
 console.log(list);
-console.log(map);
+console.log(newList);
