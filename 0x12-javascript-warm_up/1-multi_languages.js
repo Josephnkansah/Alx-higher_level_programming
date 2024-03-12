a5394183e55950b1c0844a0be20e2802586bffc7
Josephnkansah <jnkansahoseph4@gmail.com>
2024-03-12 14:21:54 -0700

    java

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+// JS to print multiple lines
 console.log('C is fun');
 console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log('Javascript is amazing');
