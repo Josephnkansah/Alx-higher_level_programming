a3ee8c7b966dc30afc8c48e48c47ba5e987db426
Josephnkansah <jnkansahoseph4@gmail.com>
2024-03-12 14:23:45 -0700

    java

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-let i;
-let count = 0;
-for (i = 0; i < process.argv.length; i++) {
-  count++;
-}
-if (count < 3) {
+// JS to check arguments passed into script
+'use strict';
+let len = process.argv.length;
+if (len < 3) {
   console.log('No argument');
-} else if (count === 3) {
-  console.log('Argument found');
-} else {
+} else if (len > 3) {
   console.log('Arguments found');
+} else {
+  console.log('Argument found');
 }
